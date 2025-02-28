c93db3cdf4310677a1ee7aaab94e5c037abcf47e
morannlx <2152533805@qq.com>
2025-02-28 22:32:19 +0800

    Site updated: 2025-02-28 22:32:18

---
+++ js/background.js
@@ -0,0 +1,17 @@
+document
+
+  .querySelector('#web_bg')
+
+  .setAttribute('style', `background-image: ${document.querySelector('.banner').style.background.split(' ')[0]};position: fixed;width: 100%;height: 100%;z-index: -1;background-size: cover;`);
+
+document
+
+  .querySelector("#banner")
+
+  .setAttribute('style', 'background-image: url(https://ooo.0x0.ooo/2023/10/02/OnIFSx.jpg)')
+
+document
+
+  .querySelector("#banner .mask")
+
+  .setAttribute('style', 'background-color:rgba(0,0,0,0)')
